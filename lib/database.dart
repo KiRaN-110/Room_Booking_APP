d017e8518dfb7de0b7884e522450d3f11608e7e5
KiRaN-110 <116154751+KiRaN-110@users.noreply.github.com>
2023-03-29 02:33:50 +0530

    Colors

---
+++ lib/database.dart
@@ -0,0 +1,88 @@
+import 'package:firebase_database/firebase_database.dart';
+
+final database = FirebaseDatabase.instance.ref();
+final weeklyScheduleRef = database.child('/Weekly Schedule');
+final weeklySchedule = {
+  'Monday': {
+    'LHB 110': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    'LHB 308': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    // add more rooms as needed
+  },
+  'Tuesday': {
+    'LHB 110': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    'LHB 308': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+  },
+  'Wednesday': {
+    'LHB 110': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    'LHB 308': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+  },
+  'Thursday': {
+    'LHB 110': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    'LHB 308': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+  },
+  'Friday': {
+    'LHB 110': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+    'LHB 308': {
+      '8AM - 9AM': 0,
+      '9AM - 10AM': 0,
+      '10AM - 11AM': 0,
+      '11AM - 12PM': 0,
+      // add more time slots as needed
+    },
+  },
+  // add more days as needed
+};
